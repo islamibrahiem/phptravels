9d9639dbf2469a220c336f43d1e51b16888c90bc
islam ibrahiem <islamibrahiem93@gmail.com>
2020-11-20 19:49:11 +0200

    registration data without phoneNumber-How did you find us?-reCAPTCHA

--- src/main/java/pages/RegisterationPage.java
+++ src/main/java/pages/RegisterationPage.java
@@ -10,16 +10,63 @@ public class RegisterationPage extends PageBase {
 		super(driver);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@FindBy(id="inputFirstName")
 	WebElement firstName;
 	@FindBy(id="inputLastName")
 	WebElement lastName;
-	
-	
-	public void inputTheRegisterationData (String firstNameTxt,String lastNameTxt) {
+	@FindBy(id="inputEmail")
+	WebElement email;
+	@FindBy(id="inputCompanyName")
+	WebElement companyName;
+	@FindBy(id="inputAddress1")
+	WebElement addressName1;
+	@FindBy(id="inputAddress2")
+	WebElement addressName2;
+	@FindBy(id="inputCity")
+	WebElement city ;
+	@FindBy(id="stateinput")
+	WebElement state;
+	@FindBy(id="inputPostcode")
+	WebElement postcode;
+	@FindBy(id="customfield2")
+	WebElement mobileNumber;
+	@FindBy(id="inputNewPassword1")
+	WebElement password;
+	@FindBy(id="inputNewPassword2")
+	WebElement confirmPassword;
+	@FindBy(id="recaptcha-anchor-label")
+	WebElement recaptcha_checkbox;
+
+
+
+	public void inputTheRegisterationData (
+			String firstNameTxt,
+			String lastNameTxt,
+			String emailTxt,
+			String companyNameTxt,
+			String addressName1Txt,
+			String addressName2Txt,
+			String cityTxt,
+			String stateTxt,
+			String postcodeTxt,
+			String mobileNumberTxt,
+			String passwordTxt,
+			String confirmPasswordTxt
+			) {
 		setTextElementText(firstName, firstNameTxt);
 		setTextElementText(lastName, lastNameTxt);
+		setTextElementText(email, emailTxt);
+		setTextElementText(companyName, companyNameTxt);
+		setTextElementText(addressName1, addressName1Txt);
+		setTextElementText(addressName2, addressName2Txt);
+		setTextElementText(city, cityTxt);
+		setTextElementText(state, stateTxt);
+		setTextElementText(postcode, postcodeTxt);
+		setTextElementText(mobileNumber, mobileNumberTxt);
+		setTextElementText(password, passwordTxt);
+		setTextElementText(confirmPassword, confirmPasswordTxt);
+		clickButton(recaptcha_checkbox);
 	}
 
 }
--- src/test/java/tests/RegisterationPageTest.java
+++ src/test/java/tests/RegisterationPageTest.java
@@ -5,18 +5,40 @@ import org.testng.annotations.Test;
 import pages.RegisterationPage;
 
 public class RegisterationPageTest extends TestBase {
-	
+
 	String test_firstNameTxt = "ibrahiem";
 	String test_lastNameTxt = "Ali";
-	
+	String test_emailTxt = "ibrahiem@test.com";
+	String test_companyNameTxt = "IBM";
+	String test_address1Txt = "opera street";
+	String test_address2Txt = "metro street";
+	String test_cityTxt = "cairo";
+	String test_stateTxt = "cairoo";
+	String test_postcodeTxt ="123456";
+	String test_mobileNumberTxt ="12345678912";
+	String test_passwordTxt = "Test_1993";
+	String test_confirmPasswordTxt = "Test_1993";
+
 	RegisterationPage registerationPageObject;
-	
+
 	@Test
 	public void test_inputTheRegisterationData()
 	{
 		registerationPageObject = new RegisterationPage(driver);
-		registerationPageObject.inputTheRegisterationData(test_firstNameTxt,
-				                                          test_lastNameTxt);
+		registerationPageObject.inputTheRegisterationData(
+				test_firstNameTxt,
+				test_lastNameTxt,
+				test_emailTxt,
+				test_companyNameTxt,
+				test_address1Txt,
+				test_address2Txt,
+				test_cityTxt,
+				test_stateTxt,
+				test_postcodeTxt,
+				test_mobileNumberTxt,
+				test_passwordTxt,
+				test_confirmPasswordTxt
+				);
 	}
 
 }
