3ce1b8d1ef73342a0b6327335abc03424cd884ec
islamibrahiem <42051765+islamibrahiem@users.noreply.github.com>
2020-11-20 15:26:02 +0200

    Update PageBase.java

--- src/main/java/pages/PageBase.java
+++ src/main/java/pages/PageBase.java
@@ -6,10 +6,24 @@ import org.openqa.selenium.support.PageFactory;
 public class PageBase {
 
     protected WebDriver driver;
+    public JavascriptExecutor jse ;
 	
 	public PageBase(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
+	}
+	
+	protected static void clickButton(WebElement button) {
+		button.click();	
+	}
+	
+	protected static void setTextElementText(WebElement textElement , String value) {
+		textElement.sendKeys(value);
+		
+	}
+	
+	public void scrollToBottom() {
+		jse.executeScript("scrollBy(0,2500)");	
 	}
 
 }
