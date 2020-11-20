eaaedf903baf6601df0395024ac943a5cc763d12
islam ibrahiem <islamibrahiem93@gmail.com>
2020-11-20 15:34:35 +0200

    import missing classes

--- src/main/java/pages/PageBase.java
+++ src/main/java/pages/PageBase.java
@@ -1,6 +1,8 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class PageBase {
