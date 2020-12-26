c05c37fe3cfa4d93703622282412334007643b3e
Islam Ibrahim Aly Mohamed <islam.ibrahim1@ibm.com>
2020-12-26 23:07:24 +0200

    edit my profile test page

--- src/test/java/tests/TestMyAccount.java
+++ src/test/java/tests/TestMyAccount.java
@@ -1,6 +1,5 @@
 package tests;
 
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import com.github.javafaker.Faker;
@@ -11,7 +10,7 @@ import pages.MyAccountPage;
 import pages.MyProfile;
 import pages.RegistrationPage;
 
-public class TestMyProfile extends TestBase{
+public class TestMyAccount extends TestBase{
 
 	HomePage homePageObject ;
 	RegistrationPage registrationPageObject ;
@@ -22,16 +21,9 @@ public class TestMyProfile extends TestBase{
 	Faker fakeData = new Faker();
 	String test_firstNameTxt = fakeData.name().username();
 	String test_lastNameTxt = fakeData.name().lastName();
+	String test_phoneNumber = fakeData.number().digits(11).toString();
 	String test_emailTxt = fakeData.internet().emailAddress();
 	String test_password = fakeData.number().digits(10).toString();
-	String test_adress1 = fakeData.name().title();
-	String test_adress2 = fakeData.name().title();
-	String test_city = "cairo";
-	String test_state = "cairo";
-	String test_zipcode = fakeData.number().digits(5).toString();
-	String test_phoneNumber = fakeData.number().digits(11).toString();
-
-
 
 	@Test
 	public void test_inputBlogResgistrationData () throws InterruptedException 
@@ -57,23 +49,11 @@ public class TestMyProfile extends TestBase{
 		loginPageObject = new LoginPage(driver);
 		loginPageObject.login(test_emailTxt, test_password);
 
-
 		myAccountPageObject.openMyProfile();
 
-		myprofilepageObject = new MyProfile(driver);
 
-		myprofilepageObject.editMyProfileData(test_firstNameTxt,
-				test_lastNameTxt,
-				test_emailTxt,
-				test_password,
-				test_password,
-				test_adress1, 
-				test_adress2, 
-				test_city,
-				test_state,
-				test_zipcode,
-				test_phoneNumber);
-		//myprofilepageObject.clicksubmit();
 	}
 
+
 }
+
