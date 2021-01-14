fe16eb9e639bd452595f8cdc9acbc1ca88393665
islamibrahiem <islamibrahiem93@gmail.com>
2021-01-14 19:16:34 +0200

    add webdriver Manager

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -26,16 +26,12 @@ public class TestBase {
 		if(browserName.equalsIgnoreCase("firefox"))
 		{
 			WebDriverManager.firefoxdriver().setup();
-			//System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver");
-			//System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"\\drivers\\geckodriver.exe");
 			driver = new FirefoxDriver();
 		}
 		else if (browserName.equalsIgnoreCase("chrome"))
 		{
 
 			WebDriverManager.chromedriver().setup();
-			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/drivers/chromedriver");
-			//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
 			driver = new ChromeDriver();
 		}
 		driver.manage().window().maximize();
